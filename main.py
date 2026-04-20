c91b7a83813f183262111802a9e6feeb8e3cc881
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-04-20 19:04:57 +0500

    added the logic to download the specified quality and ffmpeg to join video and audio stream

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import yt_dlp
 
-url = "https://www.youtube.com/watch?v=GZkbK1NXuas"
-
 def get_info(url):
     with yt_dlp.YoutubeDL({"quiet": True,"no_warnings": True,}) as ydl:
         info = ydl.extract_info(url, download=False)
 
     return info
 
-def download(url , destination):
+def download(url , destination , quality):
     ydl_opts = {
         "quiet": True,
         "no_warnings": True,
-        'outtmpl': f'{destination}/%(title)s.%(ext)s'
-    }
+        'format': f'bestvideo[height={quality}]+bestaudio/best',
+        'outtmpl': f'{destination}/%(title)s.%(ext)s',
+        'ffmpeg_location': './ffmpeg/bin/ffmpeg.exe'
+    }   
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
@@ -28,20 +28,23 @@ def get_quality(info):
     sorted_list = list(qualities)
     sorted_list.sort()
     return sorted_list
+
 def main():
     url = input("Enter the url: ")
     destination = input("Enter the destination path : ")
     info = get_info(url)
-    print(get_quality(info))
+    avaliable_quality = get_quality(info)
+    while True:
+        quality = int(input(f"Following video qualities are avaliable for the video {avaliable_quality} \n Select one : "))
+        if quality in avaliable_quality:
+            break
     title = info.get("title")
     print(f"Downloading : {title}")
-    download(url , destination)
+    download(url , destination , quality)
 
 if __name__ == "__main__":
     main()
 
-
-#add the task to select the specific quality to download
 #add the option to select between playlist and video
 #download all the videos in the playlist
 #using streamlit make an interactive gui
