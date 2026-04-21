726ee9f216fd46c5883c959775e06c6c8063323a
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-04-21 19:51:26 +0500

    done playlist featuere

--- main.py
+++ main.py
@@ -6,21 +6,24 @@ def get_info(url):
 
     return info
 
-def download(url , destination , quality):
-    ydl_opts = {
+def download_video(url , destination ,quality):
+    video_opts = {
         "quiet": True,
         "no_warnings": True,
         'format': f'bestvideo[height={quality}]+bestaudio/best',
         'outtmpl': f'{destination}/%(title)s.%(ext)s',
-        'ffmpeg_location': './ffmpeg/bin/ffmpeg.exe'
-    }   
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        'ffmpeg_location': './ffmpeg/bin/ffmpeg.exe',
+
+    }
+
+    with yt_dlp.YoutubeDL(video_opts) as ydl:
         ydl.download([url])
+    
+
 
 def get_quality(info):
     qualities = set()
     formats = info.get("formats" , [])
-
     for f in formats:
         if f != None and f.get("ext") == "mp4":
             qualities.add(f.get("height"))
@@ -29,8 +32,7 @@ def get_quality(info):
     sorted_list.sort()
     return sorted_list
 
-def main():
-    url = input("Enter the url: ")
+def get_video(url):
     destination = input("Enter the destination path : ")
     info = get_info(url)
     avaliable_quality = get_quality(info)
@@ -40,11 +42,41 @@ def main():
             break
     title = info.get("title")
     print(f"Downloading : {title}")
-    download(url , destination , quality)
+    download_video(url , destination ,quality )
+
+def download_playlist(url , destination , folder_name):
+    playlist_opts = {
+    "quiet": True,
+    "no_warnings": True,
+    'format': 'bestvideo+bestaudio/best',
+    'ignoreerrors': True,
+    'noplaylist': False,
+    'outtmpl': f'{destination}/{folder_name}/%(title)s.%(ext)s',
+    'merge_output_format': 'mp4',
+    'continuedl': True,
+    'download_archive': 'downloaded.txt',    
+    }
+
+    with yt_dlp.YoutubeDL(playlist_opts) as ydl:
+        ydl.download([url])
+
+def get_playlist(url):
+    destination = input("Enter the destination path : ")
+    folder_name = input("Enter the name of the folder for playlist : ")
+
+    print("Downloading")
+    download_playlist(url , destination , folder_name)
+
+def main():
+    url = input("Enter the url: ")
+
+    if "list=" in url: 
+        get_playlist(url)
+    else:
+        get_video(url)
+
 
 if __name__ == "__main__":
     main()
 
-#add the option to select between playlist and video
-#download all the videos in the playlist
 #using streamlit make an interactive gui
